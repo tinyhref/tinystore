c02b3b2584c5a1f93b1e269cc317eb257c3b456d
doong <donglh@gviet.vn>
2026-05-16 20:19:17 +0700

    update createStore setState

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,15 +1,25 @@
 import type { Store, Handlers, Listener } from './types';
 
+const is = (x: any, y: any) => {
+  return ((x === y && (x !== 0 || 1 / x === 1 / y)) || (x !== x && y !== y));
+}
+
+export const objectIs: ((x: any, y: any) => boolean) = typeof Object.is === 'function' ? Object.is : is;
+
 export const createStore = <T extends object, H>(initialState: T, handlersFn?: Handlers<T, H>): Store<T, H> => {
   let state = initialState;
-  const listeners = new Set<Listener>();
+  const listeners = new Set<Listener<T>>();
 
-  const setState: Store<T, H>['setState'] = (newState) => {
-    if (typeof newState === 'function') {
-      newState = newState(state);
+  const setState: Store<T, H>['setState'] = (updateState, params: { replace?: boolean } = {}) => {
+    const { replace } = params;
+
+    const nextState = typeof updateState === 'function' ? updateState(state) : updateState;
+
+    if (nextState && !objectIs(nextState, state)) {
+      const prevState = state;
+      state = replace ? nextState as T : { ...state, ...nextState };
+      listeners.forEach((listener) => listener(state, prevState));
     }
-    state = { ...state, ...newState };
-    listeners.forEach((listener) => listener());
   };
 
   let handlers: H | undefined;
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
-export type Listener = () => void;
+export type Listener<T> = (state: T, prevState: T) => void;
 
 export type Handlers<T, H = any> = (store: { state: T; setState: Store<T, H>['setState'] }) => H;
 
 export interface Store<T, H> {
   getState: () => T;
   setState: (newState: Partial<T> | ((prevState: T) => Partial<T>)) => void;
-  subscribe: (listener: Listener) => () => void;
+  subscribe: (listener: Listener<T>) => () => void;
   handlers?: H;
 }
